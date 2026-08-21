87582dbf11984a3dc8915e0f91f3c85903ed09b1
Twoot <inenquser@gmail.com>
2026-08-21 10:13:25 +0300

    Add tests for register and unregister ticket

--- tests/services/test_events_provider_client.py
+++ tests/services/test_events_provider_client.py
@@ -4,6 +4,7 @@ from unittest.mock import AsyncMock, Mock
 import httpx
 import pytest
 
+from src.schemas.ticket import TicketRegistration
 from src.services.events_provider_client import EventsProviderClient
 
 
@@ -94,4 +95,59 @@ async def test_get_free_seats_calls_client_with_correct_params():
     result = await client.get_free_seats(event_id)
 
     fake_client.get.assert_called_once_with(f"/api/events/{event_id}/seats/")
+    assert result == fake_response.json.return_value
+
+
+async def test_register_calls_client_with_correct_params():
+    payload = TicketRegistration(
+        event_id=uuid.uuid4(),
+        first_name="Ivan",
+        last_name="Ivanov",
+        email="example@example.com",
+        seat="A15",
+    )
+
+    fake_ticket_id = uuid.uuid4()
+    fake_response = Mock()
+    fake_response.json.return_value = {"ticket_id": str(fake_ticket_id)}
+
+    fake_client = AsyncMock()
+    fake_client.post.return_value = fake_response
+
+    client = EventsProviderClient(fake_client)
+
+    event_id = uuid.uuid4()
+    result = await client.register(event_id, payload)
+
+    fake_client.post.assert_called_once_with(
+        f"/api/events/{event_id}/register/",
+        json={
+            "first_name": payload.first_name,
+            "last_name": payload.last_name,
+            "seat": payload.seat,
+            "email": payload.email,
+        },
+    )
+    assert result == fake_ticket_id
+
+
+async def test_unregister_calls_client_with_correct_params():
+    fake_response = Mock()
+    fake_response.json.return_value = {"success": True}
+
+    fake_client = AsyncMock()
+    fake_client.request.return_value = fake_response
+
+    client = EventsProviderClient(fake_client)
+
+    fake_event_id = uuid.uuid4()
+    fake_ticket_id = uuid.uuid4()
+
+    result = await client.unregister(fake_event_id, fake_ticket_id)
+
+    fake_client.request.assert_called_once_with(
+        "DELETE",
+        f"/api/events/{fake_event_id}/unregister/",
+        json={"ticket_id": str(fake_ticket_id)},
+    )
     assert result == fake_response.json.return_value
