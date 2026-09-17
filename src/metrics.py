6101a39fa8204da08caf1316e2016121e768ff51
Twoot <inenquser@gmail.com>
2026-09-17 14:00:04 +0300

    Remove 10.0 bucket

--- src/metrics.py
+++ src/metrics.py
@@ -10,5 +10,5 @@ http_request_duration_seconds = Histogram(
     "http_request_duration_seconds",
     "HTTP request duration",
     ["method", "endpoint"],
-    buckets=[0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0],
+    buckets=[0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0],
 )
